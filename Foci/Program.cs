6fb217f1d2627a05a775624e96fe7e54b0c7ecbf
LoneHusko <90392521+LoneHusko@users.noreply.github.com>
2024-09-10 17:22:12 +0200

    3. feladat: a két félidőben ellentétes eredmenyű meccsek kikeresése

--- Foci/Program.cs
+++ Foci/Program.cs
@@ -9,5 +9,21 @@ class Program {
         foreach (var meccs in keres) {
             Console.WriteLine($"{meccs.HazaiCsapatNeve}-{meccs.VendegCsapatNeve}: {meccs.HazaiGol}-{meccs.VendegGol} ({meccs.ElsoFelidoHazaiGolok}-{meccs.ElsoFelidoVendegGolok})");
         }
+        
+        var forditottEredmenyek = meccsek.Where(meccs => 
+                (meccs.ElsoFelidoHazaiGolok < meccs.ElsoFelidoVendegGolok && meccs.HazaiGol > meccs.VendegGol) 
+                || 
+                (meccs.ElsoFelidoVendegGolok < meccs.ElsoFelidoHazaiGolok && meccs.VendegGol > meccs.HazaiGol)
+            )
+            .Select(meccs => new 
+            { 
+                forduloSzama = meccs.ForduloSzama, 
+                nyertesCsapat = meccs.HazaiGol > meccs.VendegGol ? meccs.HazaiCsapatNeve : meccs.VendegCsapatNeve 
+            })
+            .ToList();
+        Console.WriteLine("\n2. félidőben fordított meccsek:");
+        foreach (var result in forditottEredmenyek) {
+            Console.WriteLine($"{result.forduloSzama}.: {result.nyertesCsapat}");
+        }
     }
 }
