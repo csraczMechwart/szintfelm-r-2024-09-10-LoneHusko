7866f074042790822eb8d88fabb0ddf9a7abaf01
LoneHusko <90392521+LoneHusko@users.noreply.github.com>
2024-09-10 16:58:09 +0200

    1. feladat: mecssek beolvasása

---
+++ Foci/FileManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Foci;
+
+public class FileManager {
+    public static List<Meccs> Beolvas() {
+        List<Meccs> meccsLista = new List<Meccs>();
+
+        StreamReader sr = new StreamReader("Feladat/meccs.txt");
+        var line = sr.ReadLine();
+
+        while (line != null) {
+            var adatok = line.Split(" ");
+            if (adatok.Length == 7)
+                meccsLista.Add(new Meccs(
+                    Convert.ToInt32(adatok[0]), 
+                    Convert.ToInt32(adatok[1]), 
+                    Convert.ToInt32(adatok[2]), 
+                    Convert.ToInt32(adatok[3]), 
+                    Convert.ToInt32(adatok[4]), 
+                    adatok[5], 
+                    adatok[6]
+                ));
+
+            line = sr.ReadLine();
+        }
+        sr.Close();
+        return meccsLista;
+    }
+}
---
+++ Foci/Meccs.cs
@@ -0,0 +1,21 @@
+﻿namespace Foci;
+
+public class Meccs {
+    public Meccs(int forduloSzama, int hazaiGol, int vendegGol, int elsoFelidoGolok, int masodikFelidoGolok, string hazaiCsapatNeve, string vendegCsapatNeve) {
+        ForduloSzama = forduloSzama;
+        HazaiGol = hazaiGol;
+        VendegGol = vendegGol;
+        ElsoFelidoGolok = elsoFelidoGolok;
+        MasodikFelidoGolok = masodikFelidoGolok;
+        HazaiCsapatNeve = hazaiCsapatNeve;
+        VendegCsapatNeve = vendegCsapatNeve;
+    }
+
+    public int ForduloSzama { get; set; }
+    public int HazaiGol { get; set; }
+    public int VendegGol { get; set; }
+    public int ElsoFelidoGolok { get; set; }
+    public int MasodikFelidoGolok { get; set; }
+    public string HazaiCsapatNeve { get; set; }
+    public string VendegCsapatNeve { get; set; }
+}
